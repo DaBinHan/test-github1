03dd3c18394f38c01c0dea6f92f4a5952b1c381b
hjhjpark94 <hjhjpark9400@gmail.com>
2017-08-09 14:32:05 +0900

    add minus() method with GPU

--- calculater2.java
+++ calculater2.java
@@ -12,7 +12,13 @@ public class calculater{
   private static int sum(int a,int b){
     return a+b;
   }
-  private static int minus(int a,int b){
-    return a-b;
+  private static int minus(int a,int b,Object GPU){
+    int result=0;
+    if(GPU==null){
+      result=a-b;
+    }else{
+      result=a-b;
+    }
+    return result;
   }
 }
